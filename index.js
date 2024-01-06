6481761b241a4537af3669f341aab48a877bcfad
judy_her <judith85dm@ucla.edu>
2024-01-05 16:30:00 -0800

    added function logic to questions, writeToFile, and init

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+  inquirer.prompt([{ name: 'name', type: 'input', message: '' }]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('File Created Sucessfully');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  //call questions function here
+  //call writeToFile function here
+  const answers = await questions();
+  writeToFile(answers.fileName, answers.data);
+}
 
 // Function call to initialize app
 init();
